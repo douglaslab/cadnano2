aeb378c54bce83032f9502509974c13949479c7c
Shawn Douglas <shawn.douglas@gmail.com>
2022-12-15 10:39:46 -0800

    Bump to 2.4.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='cadnano2',
-    version='2.4.8',    
+    version='2.4.9',    
     description='Cadnano2 for PyQt6',
     long_description=long_description,
     long_description_content_type="text/markdown",
